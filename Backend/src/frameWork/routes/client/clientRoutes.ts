2dfdd3a26872170822ea1ffcb55e1d71667fa79e
Dhyan <dhyan@gmail.com>
2025-11-20 01:45:16 +0530

    forgotPassword otp sended completed

--- Backend/src/frameWork/routes/client/clientRoutes.ts
+++ Backend/src/frameWork/routes/client/clientRoutes.ts
@@ -1,31 +1,31 @@
 import { Request, Response, Router } from "express";
-import { clientLogin, clientRegisterController, sendOtpController } from "../../DI/clientInject";
+import { clientLogin, clientRegisterController, sendOtpController, sendOtpForgotPasswordController } from "../../DI/clientInject";
 
-export class UserRoutes{
-    public UserRoutes:Router
-    constructor(){
-        this.UserRoutes=Router()
+export class UserRoutes {
+    public UserRoutes: Router
+    constructor() {
+        this.UserRoutes = Router()
         this._setRoutes()
     }
 
-    private _setRoutes(){
-        this.UserRoutes.post("/signup",(req:Request,res:Response)=>{
+    private _setRoutes() {
+        this.UserRoutes.post("/signup", (req: Request, res: Response) => {
             console.log("passed 1st url")
-        sendOtpController.sendOtp(req,res)
+            sendOtpController.sendOtp(req, res)
         })
 
-        this.UserRoutes.post("/verifyOtp",(req:Request,res:Response)=>{
-            clientRegisterController.register(req,res)
+        this.UserRoutes.post("/verifyOtp", (req: Request, res: Response) => {
+            clientRegisterController.register(req, res)
         })
 
-        this.UserRoutes.post("/login",(req:Request,res:Response)=>{
+        this.UserRoutes.post("/login", (req: Request, res: Response) => {
             console.log("route called")
-            clientLogin.handleLogin(req,res)
+            clientLogin.handleLogin(req, res)
         })
 
-        // this.UserRoutes.post("/forgotpassword",(req:Request,res:Response)=>{
-        //     sendOtpForgotPasswordController.handleForgotPassword(req,res)
-        // })
+        this.UserRoutes.post("/forgotpassword", (req: Request, res: Response) => {
+            sendOtpForgotPasswordController.handleForgotPassword(req, res)
+        })
     }
 
 
