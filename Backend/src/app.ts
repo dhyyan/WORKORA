45ce77ff668aed75563ff15058aad3e2318c849f
dhyyan <dhyanpramod2@gmail.com>
2026-05-06 00:02:23 +0530

    update the stripe secrte key

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -30,10 +30,13 @@ export class App {
         this._app.use(helmet())
         this._port = process.env.PORT || 3560
         this.httpServer = createServer(this._app)
+        const origin = process.env.FRONTEND_URL || "http://localhost:5173";
+        
         this.io = new SocketIoServer(this.httpServer, {
             cors: {
-                origin: process.env.FRONTEND_URL || "http://localhost:5173",
-                credentials: true
+                origin: origin,
+                credentials: true,
+                methods: ["GET", "POST"]
             }
         })
 
@@ -41,8 +44,9 @@ export class App {
         this._database.connectDB()
         this._app.use(
             cors({
-                origin: process.env.FRONTEND_URL || "http://localhost:5173",
-                credentials: true
+                origin: origin,
+                credentials: true,
+                methods: ["GET", "POST", "PUT", "PATCH", "DELETE"]
             })
         )
         this._app.use((req, res, next) => {
